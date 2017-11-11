120df90a0af85b783f828f12d7f1e1d672b16f4f
zhenqing <zhaozhenqing@gmail.com>
2017-11-10 16:06:16 -0800

    change to https

--- utils/api.js
+++ utils/api.js
@@ -2,7 +2,7 @@ var axios = require('axios')
 
 module.exports = {
     fetchAccountLogs: function(account, country, account_name) {
-        var encodeURI = window.encodeURI('http://a9test.ibport.com/api/v1/account_logs?country=' + country + '&account=' + account + '&account_name=' + account_name +'&access_token=b41127cf658eeb348ebc5a9513826bb0')
+        var encodeURI = window.encodeURI('https://a9test.ibport.com/api/v1/account_logs?country=' + country + '&account=' + account + '&account_name=' + account_name +'&access_token=b41127cf658eeb348ebc5a9513826bb0')
         return axios.get(encodeURI)
             .then(function (response) {
                 return response.data;
